47e2c6e0288b4dae871d14abb51141583b8e9559
Alejo-reyes1 <alejoplayer1@gmail.com>
2025-08-28 20:29:24 -0500

    create

---
+++ util/util.ex
@@ -0,0 +1,25 @@
+defmodule Util do
+
+  def show_message(message) do
+    message
+    |> IO.puts()
+end
+
+def input(message, :string) do
+System.cmd("java", ["-cp", ".", "Mensaje","input", message])
+|> elem(0)
+|> String.trim()
+
+end
+
+def input(message, :integer) do
+input(message, :string)
+|> String.to_integer()
+end
+
+def input(message, :float) do
+input(message, :string)
+|> String.to_float()
+end
+
+end
